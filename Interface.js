681b72b7864bee7174e221e64d5717f508c9e697
Onose Inyinbor <108410914+OnoseI@users.noreply.github.com>
2022-12-28 19:57:11 -0500

    Add files via upload

---
+++ Interface.js
@@ -0,0 +1,37 @@
+//TASK 1
+interface heroInterface{    //normally I would creat a seperate file for the interface.
+    name:string;            //here I created important charachteristics of a hero.
+    specialty:string;
+    weakness:string;
+    age:number;
+  }
+  const heroArray:heroInterface[]=[
+    {
+      name:'Zuko',          //here I assigned the charachteristics in an array belonging to the hero
+      specialty:'fire',
+      weakness:'extreme cold',
+      age:16,
+    }
+  ] 
+  //interface can only be used on typescript files. Not sure what typescript is
+  //I love this because I just found out that I cannot implement interfaces on javascript so I would have to use typescript instead. Though I found this assessment challenging I have learned so much mingling through each and every step. They are all connected and not as hard as on may think once they get the hang of it. 
+  //I am still going to keep my code because in the directions it does not say we need typescript. I have downloaded a lot of new programs on my laptop and I do not want to download anything that is not in the directions so I will not mess anything up. Please keep in mind this is all very new to me. 
+  
+  //Task 2
+{       
+    function myFunction(quest){
+        return quest;
+    }
+    //I created a function to call and return the quest so when the user calls for it. It will return with the description.
+    //I think I am missing something. Something where I can give the description and it will call it.
+    //I might have to do another array for the quest as well.
+}
+
+  //TASK 3
+     //Task 3 confused me because I am not sure if we are simply updating the quest. Or show that the quest has been updated. If that makes sense
+     //Coding wise I would not know how to show that. Unless I just update the quest but not showing that it has been updated.
+
+  //TASK 4   
+    //Same thing with task 4 as in task 3. Are we deleting the quest. Or showing that the quest has been deleted.
+    //I'm not sure how to display this unfortunately.
+    //I would appreciate to ask one of the developers to show me how this can be done.
